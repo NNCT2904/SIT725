aeb08c5e1c52d866927d91db1b0ab15ab00a07a7
ThanhNguyen <nnct2000@gmail.com>
2022-03-09 20:57:53 +1100

    Setup MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 var cors = require('cors');
 var exress = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var appRouter = require('./routes/routes.js');
-var PORT = 3000
+var PORT = 3000;
 
 var app = exress();
 app.use(cors());
 app.use(exress.static('./public'));
 app.use(appRouter);
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}, (err) => {
+    console.log('Cannot connect to MongoDb: ' + err);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
