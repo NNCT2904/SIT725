4e21937b3abe4405fe09ddfc4f2219034267b320
ThanhNguyen <nnct2000@gmail.com>
2022-03-16 19:46:45 +1100

    Refactor Adder

--- backend/src/routes/controllers/adder.js
+++ backend/src/routes/controllers/adder.js
@@ -1,9 +1,10 @@
 // Example: /adder?num1=1&num2=2
 const adder = (req, res) => {
-    var num1 = parseInt(req.query.num1);
-    var num2 = parseInt(req.query.num2);
-    var result = num1 + num2;
-    res.send(`${num1} + ${num2} = ${result}`)
-}
+  var num1 = parseInt(req.body.num1);
+  var num2 = parseInt(req.body.num2);
+
+  var result = num1 + num2;
+  res.send(`${num1} + ${num2} = ${result}`);
+};
 
 module.exports = adder;
